d86bbf1c317577ce5684fd844d856ebb8e0dae79
Jimmy <j.austin.ozu@gmail.com>
2023-06-16 21:22:07 +1200

    Create version 0.0.1 of the Odin Calculator
    Publish the code for the first working version of the Calculator for the
    Odin Project. The Calculator can currently perform the basic arithmetic
    by a user inputing a number, operand and number through some buttons,
    and a result being displayed as the innerText of an h1 element. In the
    next version, I'll be looking into being able to show the previous
    equation and being able to handle decimal numbers as inputs. Styling of
    the Calculator is bare bones right now, I want to focus on the code.

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-console.log('hello world');
+console.log("hello world");
+
+
+
+const display = document.querySelector("#display-screen");
+const mathInput = document.querySelectorAll(".input");
+
+mathInput.forEach((input) => {
+  input.addEventListener("click", () => {
+    display.innerText += input.innerText;
+  });
+});
+
+const equate = document.getElementById("equate");
+
+equate.addEventListener("click", () => {
+    const equation = display.innerText.split('')
+  const firstNumber = equation[0];
+  const operator = equation[1];
+  const secondNumber = equation[2];
+  const result = operate(operator, firstNumber, secondNumber);
+  display.innerText = result;
+});
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+  display.innerText = "";
+});
+
+
+const add = (a, b) => {
+    return a + b;
+  };
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  const multiply = (a, b) => {
+    return a * b;
+  };
+  const divide = (a, b) => {
+    return a / b;
+  };
+  
+  const operate = (operator, a, b) => {
+      a = Number(a)
+      b = Number(b)
+    switch (operator) {
+      case "+":
+        return add(a, b);
+      case "-":
+        return subtract(a, b);
+      case "*":
+        return multiply(a, b);
+      case "/":
+        return divide(a, b);
+      default:
+        return null;
+    }
+  };
